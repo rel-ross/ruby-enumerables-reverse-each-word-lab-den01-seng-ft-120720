ad5708ad6cc837514b9164237168f12005d02d4b
Arielle ross <arielle@arielleross.com>
2020-11-26 17:55:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 require 'pry'
 def reverse_each_word(sentence)
   string_array = sentence.split(" ")
-  p string_array
- puts string_array.collect {|string| string.reverse}
+ p string_array.collect {|string| string.reverse}
 
 end
 
