4620586b564da8c3c7f75757cfefa8c5677e4af9
Arielle ross <arielle@arielleross.com>
2020-11-26 17:52:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 def reverse_each_word(sentence)
   string_array = sentence.split(" ")
   p string_array
- reverse_string = string_array.each {|word| "#{word}!"}
+ reverse_string = string_array.each {|string| "#{string}!"}
  puts reverse_string
 end
 
