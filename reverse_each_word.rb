ff83d4b1b41da34827e6e974036560219b44ef38
Arielle ross <arielle@arielleross.com>
2020-11-26 18:02:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
 
-sentence1="Hello there, and how are you?"
 def reverse_each_word(sentence)
   string_array = sentence.split(" ")
   reversed_array = string_array.collect {|string| string.reverse}
   new_reversed_string = reversed_array.join(" ")
   puts new_reversed_string
 end
-
-reverse_each_word(sentence1)
