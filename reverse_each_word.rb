562665f2841e0d175012bb74af7a4f40e6d9315d
Arielle ross <arielle@arielleross.com>
2020-11-26 18:01:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@
 sentence1="Hello there, and how are you?"
 def reverse_each_word(sentence)
   string_array = sentence.split(" ")
- print string_array.collect {|string| string.reverse}
+  reversed_array = string_array.collect {|string| string.reverse}
+  new_reversed_string = reversed_array.toString
 end
 
 reverse_each_word(sentence1)
