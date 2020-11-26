32f676e8da94de29feb3a3b41e891ef17acd9172
Arielle ross <arielle@arielleross.com>
2020-11-26 18:02:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,5 @@ def reverse_each_word(sentence)
   string_array = sentence.split(" ")
   reversed_array = string_array.collect {|string| string.reverse}
   new_reversed_string = reversed_array.join(" ")
-  print new_reversed_string
+  p new_reversed_string
 end
