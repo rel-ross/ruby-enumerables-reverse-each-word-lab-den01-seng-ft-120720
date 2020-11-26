a6feefcebad05928ea84b63801caa441d6064687
Arielle ross <arielle@arielleross.com>
2020-11-26 17:53:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   string_array = sentence.split(" ")
   p string_array
  puts string_array.each {|string| "#{string}!"}
- p reverse_string
+
 end
 
 reverse_each_word("hi, I'm Arielle")
